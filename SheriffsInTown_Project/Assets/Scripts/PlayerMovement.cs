d963db9fad853f61570837ff4be242d74da46e2c
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-03-08 01:28:31 +0100

    Aggiornato PlayerMovement.cs
    Il giocatore può muoversi liberamente sulla scena seguendo il tutorial di Kevin

--- SheriffsInTown_Project/Assets/Scripts/PlayerMovement.cs
+++ SheriffsInTown_Project/Assets/Scripts/PlayerMovement.cs
@@ -2,45 +2,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Riferimenti
-    CharacterController controller;
-    Camera cam;
-    
-    [Header("Parametri movimento")]
-    [SerializeField] float _baseMoveSpeed = 10f;
-    public float speedMultiplier = 1f;
+    [Tooltip("Velocita' di movimento del personaggio")]
+    [SerializeField] float movementSpeed = 10f;
+    [Tooltip("Quanto velocemente ruota il personaggio verso una nuova direzione")]
+    [SerializeField] [Range(0f,1f)] float smoothRotationFactor = .1f;
+    Vector3 input;  //Usato per memorizzare i valori di input di movimento del giocatore
 
-    Vector3 input;
+    Camera cam; //Utilizzato per rendere il movimento dipendente dall'orientamento della camera
+    CharacterController controller; //Utilizzato per muovere il personaggio
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //Prendo i riferimenti necessari
         cam = Camera.main;
+        controller = GetComponent<CharacterController>();
 
-        GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;     //Chiamo il metodo quando il giocatore mette in pausa il gioco o lo riprende
+        //Chiamo il metodo quando il giocatore mette in pausa il gioco o lo riprende
+        GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+
         //GameManager.PlayerWonGame += () => enabled = false;
-    }
-    private void OnGameStateChanged(GameState newGameState)
-    {
-        enabled = newGameState == GameState.Gameplay;
     }
 
     void Update()
     {
+        //Salvo gli input del giocatore nel vettore
         input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 moveDir = (Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * input).normalized; //Stabilisci la direzione del movimento in base alla camera
-        controller.SimpleMove(moveDir * _baseMoveSpeed * speedMultiplier);    //Muovi il giocatore con la gravita' automatica attiva
 
-        if (input.magnitude > 0)
+        //Determino l'orientamento di movimento del giocatore facendo ruotare di "cam.transform.eulerAngles.y" gradi il vettore di input
+        Vector3 moveDir = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * input;
+
+        controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+        //Faccio ruotare il personaggio solo quando il giocatore preme uno o piu tasti di input di movimento
+        if (input.magnitude >= .1f)
         {
-            Quaternion target = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, .1f);
+            //Stabilisco quanto e' l'angolo presente tra il movimento frontale e laterale
+            float targetRotationAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+
+            //Salvo la rotazione da raggiungere in una variabile di appoggio
+            Quaternion targetRotation = Quaternion.Euler(0, targetRotationAngle, 0);
+
+            //Smorzo la rotazione del personaggio dalla rotazione attuale a quella da raggiungere
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothRotationFactor);
         }
+
     }
 
     private void OnDestroy()
     {
+        //Annullo l'ascolto all'evento della pausa di gioco
         GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
         //GameManager.PlayerWonGame -= () => enabled = false;
+    }
+
+    private void OnGameStateChanged(GameState newGameState)
+    {
+        //Se il gioco non e' in pausa, questo componente e' attivo
+        enabled = newGameState == GameState.Gameplay;
     }
 }
