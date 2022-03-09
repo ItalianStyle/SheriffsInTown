d6fecf7c38f3638f9f2c24fb7aab11a453bc9315
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-03-09 12:03:57 +0100

    Setup GameManager.cs
    Ora il GameManager non si distrugge tra le scene e riavvia il gioco in caso di morte del giocatore

--- SheriffsInTown_Project/Assets/Scripts/GameManager.cs
+++ SheriffsInTown_Project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+    private void OnEnable()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
-        
+        switch(scene.buildIndex)
+        {
+            case 0:
+                PlayerHealthSystem.OnPlayerDead += () => SceneManager.LoadScene(0);
+                break;
+        }
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealthSystem.OnPlayerDead -= () => SceneManager.LoadScene(0);
     }
 }
