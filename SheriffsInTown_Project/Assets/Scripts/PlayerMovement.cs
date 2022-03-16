d1dd40ea4573faabc89ad4eceb2842697dbdb473
ItalianStyle <lorenzo.petraroli@hotmail.it>
2022-03-16 11:12:29 +0100

    Aggiornato PlayerMovement.cs
    - Aggiunto il namespace di SheriffsInTown per correggere un conflitto con gli esempi di Cinemachine
    - Rimossa la meccanica di iscrizione all'evento di sparo per ruotare il giocatore
    - I metodi di movimento e salto spostati nel FixedUpdate()
    - Aggiustata la funzionalità di rotazione del personaggio quando spara

--- SheriffsInTown_Project/Assets/Scripts/PlayerMovement.cs
+++ SheriffsInTown_Project/Assets/Scripts/PlayerMovement.cs
@@ -2,104 +2,111 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace SheriffsInTown
 {
-    [Header("Movimento")]
-    [Tooltip("Velocita' di movimento del personaggio")]
-    [SerializeField] float movementSpeed = 10f;
-
-    [Header("Rotazione")]
-    [Tooltip("Quanto velocemente ruota il personaggio verso una nuova direzione")]
-    [SerializeField] [Range(0f,1f)] float smoothRotationFactor = .1f;
-
-    [Header("Salto")]
-    [Tooltip("Quanta forza usa il personaggio per saltare")]
-    [SerializeField] [Min(.1f)] float jumpForce = .1f;
-
-    Vector3 input;  //Usato per memorizzare i valori di input di movimento del giocatore
-    Vector3 jumpInput = Vector3.zero;   //Utilizzato per muovere il personaggio in verticale per il salto
-    Vector3 gravity = new Vector3(0, -9.81f, 0);    //Vettore di gravità standard
-    
-    Camera cam; //Utilizzato per rendere il movimento dipendente dall'orientamento della camera
-    CharacterController controller; //Utilizzato per muovere il personaggio
-
-    private void OnEnable()
+    public class PlayerMovement : MonoBehaviour
     {
-        PlayerShooting.OnShotFired += (isPlayer) => { if (isPlayer) FaceCamera(); };
-    }
+        [Header("Movimento")]
+        [Tooltip("Velocita' di movimento del personaggio")]
+        [SerializeField] float movementSpeed = 10f;
 
-    private void Start()
-    {
-        //Prendo i riferimenti necessari
-        cam = Camera.main;
-        controller = GetComponent<CharacterController>();
+        [Header("Rotazione")]
+        [Tooltip("Quanto velocemente ruota il personaggio verso una nuova direzione")]
+        [SerializeField] [Range(0f, 1f)] float smoothRotationFactor = .1f;
 
-        //Chiamo il metodo quando il giocatore mette in pausa il gioco o lo riprende
-        GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+        [Header("Salto")]
+        [Tooltip("Quanta forza usa il personaggio per saltare")]
+        [SerializeField] [Min(.1f)] float jumpForce = .1f;
 
-        //GameManager.PlayerWonGame += () => enabled = false;
-    }
+        Vector3 input;  //Usato per memorizzare i valori di input di movimento del giocatore
+        Vector3 jumpInput = Vector3.zero;   //Utilizzato per muovere il personaggio in verticale per il salto
+        Vector3 gravity = new Vector3(0, -9.81f, 0);    //Vettore di gravità standard
 
-    void Update()
-    {
-        MovePlayer();
-        Jump();
-    }
+        Camera cam; //Utilizzato per rendere il movimento dipendente dall'orientamento della camera
+        CharacterController controller; //Utilizzato per muovere il personaggio
 
-    private void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        private void Start()
         {
-            jumpInput.y = jumpForce;
+            //Prendo i riferimenti necessari
+            cam = Camera.main;
+            controller = GetComponent<CharacterController>();
+
+            //Chiamo il metodo quando il giocatore mette in pausa il gioco o lo riprende
+            GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+
+            //GameManager.PlayerWonGame += () => enabled = false;
         }
-        if (jumpInput.y > 0)
+
+        void FixedUpdate()
         {
-            jumpInput += gravity * Time.deltaTime;
-            controller.Move(jumpInput * Time.deltaTime);
+            MovePlayer();
+            Jump();
         }
-    }
 
-    private void MovePlayer()
-    {
-        //Salvo gli input del giocatore nel vettore
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-        //Determino l'orientamento di movimento del giocatore facendo ruotare di "cam.transform.eulerAngles.y" gradi il vettore di input
-        Vector3 moveDir = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * input;
-
-        controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime + gravity * Time.deltaTime);
-
-        //Faccio ruotare il personaggio solo quando il giocatore preme uno o piu tasti di input di movimento
-        if (input.magnitude >= .1f)
+        private void Jump()
         {
-            //Stabilisco quanto e' l'angolo presente tra il movimento frontale e laterale
-            float targetRotationAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-
-            //Salvo la rotazione da raggiungere in una variabile di appoggio
-            Quaternion targetRotation = Quaternion.Euler(0, targetRotationAngle, 0);
-
-            //Smorzo la rotazione del personaggio dalla rotazione attuale a quella da raggiungere
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothRotationFactor);
+            if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+            {
+                //Definisco il vettore massimo per saltare
+                jumpInput.y = jumpForce;
+            }
+            if (jumpInput.y > 0)
+            {
+                jumpInput += gravity * Time.deltaTime;
+                controller.Move(jumpInput * Time.deltaTime);
+            }
         }
-    }
 
-    private void OnDestroy()
-    {
-        //Annullo l'ascolto all'evento della pausa di gioco
-        GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
+        private void MovePlayer()
+        {
+            //Salvo gli input del giocatore nel vettore
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        PlayerShooting.OnShotFired -= (isPlayer) => { if (isPlayer) FaceCamera(); };
-        //GameManager.PlayerWonGame -= () => enabled = false;
-    }
+            //Determino l'orientamento di movimento del giocatore facendo ruotare di "cam.transform.eulerAngles.y" gradi il vettore di input
+            Vector3 moveDir = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * input;
 
-    private void OnGameStateChanged(GameState newGameState)
-    {
-        //Se il gioco non e' in pausa, questo componente e' attivo
-        enabled = newGameState == GameState.Gameplay;
-    }
+            controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime + gravity * Time.deltaTime);
 
-    public void FaceCamera()
-    {
-        transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+            //Faccio ruotare il personaggio solo quando il giocatore preme uno o piu tasti di input di movimento
+            if (input.magnitude >= .1f && !Input.GetKey(KeyCode.Mouse0))
+            {
+                //Stabilisco quanto e' l'angolo presente tra il movimento frontale e laterale
+                float targetRotationAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+
+                //Salvo la rotazione da raggiungere in una variabile di appoggio
+                Quaternion targetRotation = Quaternion.Euler(0, targetRotationAngle, 0);
+
+                //Smorzo la rotazione del personaggio dalla rotazione attuale a quella da raggiungere
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothRotationFactor);
+            }
+            //Il giocatore ruota verso la direzione della camera quando spara
+            else if(Input.GetKey(KeyCode.Mouse0))
+            {
+                //Salvo la rotazione da raggiungere in una variabile di appoggio
+                Quaternion targetRotation = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);
+
+                //Smorzo la rotazione del personaggio dalla rotazione attuale a quella da raggiungere
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            //Annullo l'ascolto all'evento della pausa di gioco
+            GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
+
+            //GameManager.PlayerWonGame -= () => enabled = false;
+        }
+
+        private void OnGameStateChanged(GameState newGameState)
+        {
+            //Se il gioco non e' in pausa, questo componente e' attivo
+            enabled = newGameState == GameState.Gameplay;
+        }
+
+        public void FaceCamera()
+        {
+            transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+        }
     }
 }
