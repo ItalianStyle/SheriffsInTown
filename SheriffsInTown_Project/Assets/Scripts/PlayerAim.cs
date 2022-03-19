4a99d84a0c19810c1920c9f6b62bd135d59660f5
ItalianStyle <lorenzo.petraroli@hotmail.it>
2022-03-19 12:04:28 +0100

    Costruita funzionalità PlayerAim.cs
    Il componente ora gestisce la visualizzazione del mirino quando il giocatore mira ad un punto entro il range di fuoco permesso

--- SheriffsInTown_Project/Assets/Scripts/PlayerAim.cs
+++ SheriffsInTown_Project/Assets/Scripts/PlayerAim.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAim : MonoBehaviour
 {
+    [Tooltip("Mirino da utilizzare per farlo apparire quando il giocatore mira ad un punto entro il raggio")]
+    [SerializeField] Image reticle;
+
+
+    Camera cam;
+    PlayerShooting playerShooting;  //Serve per prendere i valori di range e layermask
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Prendo i riferimenti necessari
+        cam = Camera.main;
+        playerShooting = GetComponent<PlayerShooting>();
+
+        reticle.enabled = false;
+        StartCoroutine(CheckReticle());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CheckReticle()
     {
-        
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        while (true)
+        {
+            Ray ray = cam.ScreenPointToRay(screenCenter);
+            reticle.enabled = Physics.Raycast(ray, playerShooting.AttackRange, playerShooting.LayerMask);
+
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
