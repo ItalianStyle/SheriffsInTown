6d7d967f8033613484e6fa665c4a9cb13d7d5225
ItalianStyle <lorenzo.petraroli@hotmail.it>
2022-03-19 16:54:36 +0100

    Update Gate.cs
    Aggiunto un riferimento all'oggetto stesso quando il giocatore entra nella zona del cancello per essere disattivato dal KeyHolder

--- SheriffsInTown_Project/Assets/Scripts/Gate.cs
+++ SheriffsInTown_Project/Assets/Scripts/Gate.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
-    public static event Action OnPlayerNearGate = delegate { };
+    public static event Action<GameObject> OnPlayerNearGate = delegate { };
     public static event Action OnPlayerLeftGate = delegate { };
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            OnPlayerNearGate?.Invoke();
+            OnPlayerNearGate?.Invoke(gameObject);
         }
     }
 
