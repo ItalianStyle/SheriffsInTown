6eae578e66cc5d46425c1ab67bf1ad5bc5310d2e
ItalianStyle <lorenzo.petraroli@hotmail.it>
2022-03-23 12:19:07 +0100

    Aggiunta funzionalità ai bottoni (da rivedere)

--- SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
+++ SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
@@ -1,22 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
-    public int score = 0;
-    public Text text;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button playBtn;
+    [SerializeField] Button exitBtn;
+
+    private void OnEnable()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+        SceneManager.sceneLoaded += (scene, loadSceneMode) => 
+        {
+            GetReferences(scene);
+            SetupScene(scene);
+        };
+
+           
+            }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= (scene, loadSceneMode) =>
+        {
+            GetReferences(scene);
+            SetupScene(scene);
+        };
+    }
+
+    private void Start()
+    {
+        playBtn.onClick.AddListener(() =>
+        {
+            Debug.Log("Bottone cliccato");
+            SceneManager.LoadScene(1);
+        });
+
+
+        exitBtn.onClick.AddListener(() => Application.Quit());
+    }
+
+    private void SetupScene(Scene scene)
+    {
+        switch(scene.buildIndex)
+        {
+            case 0:
+                break;
+
+            case 1:
+                Cursor.lockState = CursorLockMode.Locked;
+                break;
+        }
+    }
+
+    private void GetReferences(Scene scene)
+    {
+        switch(scene.buildIndex)
+        {
+            case 0:
+                playBtn = GameObject.FindGameObjectWithTag("PlayBtn").GetComponent<Button>();
+                exitBtn = GameObject.FindGameObjectWithTag("ExitBtn").GetComponent<Button>();
+                break;
+
+            case 1:
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = "Score: " + score.ToString();
     }
 }
