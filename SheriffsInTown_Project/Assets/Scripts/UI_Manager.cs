af8b8ca6a074d166f6e9ae2259538f2345393f76
gFiamma <81176822+emanuelefazio@users.noreply.github.com>
2022-03-23 17:53:00 +0100

    Aggiornato UI_Manager.cs
    - Commentato il codice
    - Aggiunta di una verifica dell'esistenza di ogni riferimento al bottone quando si aggiungono le funzioni all'evento onClick

--- SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
+++ SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
+    [Tooltip("Riferimento al tasto per giocare/riprendere il gioco")]
     [SerializeField] Button playBtn;
+
+    [Tooltip("Riferimento al tasto per uscire dal gioco/dal menù di pausa")]
     [SerializeField] Button exitBtn;
 
     private void OnEnable()
@@ -17,9 +17,8 @@ public class UI_Manager : MonoBehaviour
             GetReferences(scene);
             SetupScene(scene);
         };
+    }
 
-           
-            }
     private void OnDisable()
     {
         SceneManager.sceneLoaded -= (scene, loadSceneMode) =>
@@ -31,16 +30,14 @@ public class UI_Manager : MonoBehaviour
 
     private void Start()
     {
-        playBtn.onClick.AddListener(() =>
-        {
-            Debug.Log("Bottone cliccato");
-            SceneManager.LoadScene(1);
-        });
+        //Carica la scena del gioco quando il giocatore preme il tasto "Gioca"
+        playBtn?.onClick.AddListener(() => SceneManager.LoadScene(1));
 
-
-        exitBtn.onClick.AddListener(() => Application.Quit());
+        //Esci dal gioco quando il giocatore preme il tasto "Esci"
+        exitBtn?.onClick.AddListener(() => Application.Quit());
     }
 
+    //Prepara i componenti in base alla scena attiva
     private void SetupScene(Scene scene)
     {
         switch(scene.buildIndex)
@@ -54,6 +51,7 @@ public class UI_Manager : MonoBehaviour
         }
     }
 
+    //Prende i riferimenti in base alla scena attiva
     private void GetReferences(Scene scene)
     {
         switch(scene.buildIndex)
