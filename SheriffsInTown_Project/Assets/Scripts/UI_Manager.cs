742fc532a52d97e7df918bf5c34ffcf19de8034a
ItalianStyle <lorenzo.petraroli@hotmail.it>
2022-03-29 15:34:44 +0200

    Aggiornato UI_Manager.cs
    - Lo UI_Manager è un'entità statica e persistente tra le scene (come il GameManager)
    - Aggiunto riferimento al pannello della pausa
    - Aggiunti settaggi iniziali per ogni scena
    - Creato il metodo HandleCursor() per gestire il cursore in base allo stato del gioco
    - Creato il metodo HandlePausePanel() per gestire il pannello della pausa in base allo stato del gioco
    - Creato il metodo statico SetCanvasGroup() per attivare/disabilitare i pannelli UI

--- SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
+++ SheriffsInTown_Project/Assets/Scripts/UI_Manager.cs
@@ -1,17 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
-    [Tooltip("Riferimento al tasto per giocare/riprendere il gioco")]
-    [SerializeField] Button playBtn;
+    //Riferimento al tasto per giocare/riprendere il gioco
+    Button playBtn;
 
-    [Tooltip("Riferimento al tasto per uscire dal gioco/dal menù di pausa")]
-    [SerializeField] Button exitBtn;
+    //Riferimento al tasto per uscire dal gioco/dal menù di pausa
+    Button exitBtn;
+
+    //Riferimento al pannello della pausa
+    CanvasGroup pausePanel;
+
+    public static UI_Manager instance;
 
     private void OnEnable()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
         SceneManager.sceneLoaded += (scene, loadSceneMode) => 
         {
             GetReferences(scene);
@@ -26,48 +41,77 @@ public class UI_Manager : MonoBehaviour
             GetReferences(scene);
             SetupScene(scene);
         };
-    }
-
-    private void Start()
-    {
-        //Carica la scena del gioco quando il giocatore preme il tasto "Gioca"
-        playBtn?.onClick.AddListener(() => SceneManager.LoadScene(1));
-
-        //Esci dal gioco quando il giocatore preme il tasto "Esci"
-        exitBtn?.onClick.AddListener(() => Application.Quit());
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            GameStateManager.Instance.OnGameStateChanged -= HandlePausePanel;
+        }
     }
 
     //Prepara i componenti in base alla scena attiva
     private void SetupScene(Scene scene)
     {
-        switch(scene.buildIndex)
+        switch (scene.buildIndex)
         {
             case 0:
+                //Carica la scena del gioco quando il giocatore preme il tasto "Gioca"
+                playBtn.onClick.AddListener(() => SceneManager.LoadScene(1));
+
+                //Esci dal gioco quando il giocatore preme il tasto "Esci"
+                exitBtn.onClick.AddListener(() => Application.Quit());
                 break;
 
             case 1:
-                Cursor.lockState = CursorLockMode.Locked;
+                SetCanvasGroup(pausePanel, false);
+                HandleCursor(GameState.Gameplay);
+
+                //Riprendi il gioco dalla pausa quando il giocatore preme il tasto "Riprendi"
+                playBtn.onClick.AddListener(() => GameStateManager.Instance.SetState(GameState.Gameplay));
+
+                //Torna al menu' quando il giocatore preme il tasto "Esci"
+                exitBtn.onClick.AddListener(() => SceneManager.LoadScene(0));
+
+                GameStateManager.Instance.OnGameStateChanged += (newGameState) => 
+                {
+                    HandleCursor(newGameState);
+                    HandlePausePanel(newGameState);
+                } ;
                 break;
         }
+    }
+
+    //Mostra e libera il cursore quando il gioco e' in pausa e viceversa
+    private void HandleCursor(GameState newGameState)
+    {
+        bool isGamePaused = newGameState is GameState.Paused;
+        Cursor.lockState =  isGamePaused ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = isGamePaused;
+    }
+
+    //Rendi visibile o meno il pannello della pausa in base al nuovo stato di gioco
+    private void HandlePausePanel(GameState newGameState)
+    {
+        SetCanvasGroup(pausePanel, newGameState is GameState.Paused);
     }
 
     //Prende i riferimenti in base alla scena attiva
     private void GetReferences(Scene scene)
     {
-        switch(scene.buildIndex)
+        playBtn = GameObject.FindGameObjectWithTag("PlayBtn").GetComponent<Button>();
+        exitBtn = GameObject.FindGameObjectWithTag("ExitBtn").GetComponent<Button>();
+        switch (scene.buildIndex)
         {
             case 0:
-                playBtn = GameObject.FindGameObjectWithTag("PlayBtn").GetComponent<Button>();
-                exitBtn = GameObject.FindGameObjectWithTag("ExitBtn").GetComponent<Button>();
                 break;
 
             case 1:
+                pausePanel = GameObject.Find("UI/PausePanel").GetComponent<CanvasGroup>();
                 break;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SetCanvasGroup(CanvasGroup canvasGroup, bool canActive)
     {
+        canvasGroup.alpha = canActive ? 1f : 0f;
+        canvasGroup.blocksRaycasts = canActive;
     }
 }
