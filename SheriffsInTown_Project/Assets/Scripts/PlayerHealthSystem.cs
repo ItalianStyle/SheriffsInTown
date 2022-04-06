83d2ab122967df2c268370db25c2f327de67c730
gFiamma <81176822+emanuelefazio@users.noreply.github.com>
2022-04-06 19:15:31 +0200

    Introdotte le vite totali nel sistema di vita del giocatore

--- SheriffsInTown_Project/Assets/Scripts/PlayerHealthSystem.cs
+++ SheriffsInTown_Project/Assets/Scripts/PlayerHealthSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class PlayerHealthSystem : MonoBehaviour
 {
+    public static event Action<int> OnLivesChanged = delegate { };
     public static event Action<GameObject> OnPlayerDead = delegate { };
     public static event Action<int, int> OnPlayerDamaged = delegate { };
     public static event Action<int, int> OnPlayerHealed = delegate { };
@@ -11,24 +12,51 @@ public class PlayerHealthSystem : MonoBehaviour
     [SerializeField] int maxHealth = 100;
     [SerializeField] int _currentHealth = 100;
 
+    [SerializeField] int _totalLives = 3;
+
+    //Vite a disposizione del giocatore prima di perdere la partita
+    int TotalLives
+    {
+        get => _totalLives;
+
+        set
+        {
+            _totalLives = value;
+            OnLivesChanged?.Invoke(_totalLives);
+        }
+    }
+
+    //Proprietà per gestire la salute del giocatore
     public int CurrentHealth
     {
         get => _currentHealth;
         set
         {
+            //Salvo la salute corrente del giocatore in una variabile dummy
             int oldHP = _currentHealth;
             _currentHealth = value;
 
+            //Invoco l'evento relativo se è stato ferito o curato
             if (oldHP > _currentHealth)
                 OnPlayerDamaged?.Invoke(_currentHealth, maxHealth);
-            
             else
                  OnPlayerHealed?.Invoke(_currentHealth, maxHealth);
 
             if (_currentHealth <= 0)
             {
-                //RespawnSystem.instance.ReloadScene();
                 OnPlayerDead?.Invoke(gameObject);
+
+                TotalLives--;
+                if (TotalLives <= 0)
+                {
+                    //Gioco perso
+                    GameStateManager.Instance.SetState(GameState.Lost);
+                }
+                else
+                {
+                    //Respawna
+                    RespawnManager.Instance.RespawnPlayer(gameObject);
+                }             
 
                 _currentHealth = maxHealth;
             }
@@ -36,6 +64,7 @@ public class PlayerHealthSystem : MonoBehaviour
             OnPlayerHealthChanged?.Invoke(_currentHealth, maxHealth);
         }
     }
+
     public static PlayerHealthSystem instance;
 
     private void OnEnable()
