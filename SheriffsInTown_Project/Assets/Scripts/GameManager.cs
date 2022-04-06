c596576e0f7cb747f98aa47d37399d078797ec61
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-04-06 15:33:07 +0200

    Aggiunta la logica della sconfitta e dei respawn limitati nel GameManager.cs

--- SheriffsInTown_Project/Assets/Scripts/GameManager.cs
+++ SheriffsInTown_Project/Assets/Scripts/GameManager.cs
@@ -1,38 +1,44 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static event Action<int> OnLivesChanged = delegate { };
+    public static event Action OnGameLost = delegate { };
+
     public static GameManager instance = null;
 
     //Riferimento al player
     GameObject player;
 
     //Vite a disposizione del giocatore prima di perdere la partita
-    int totalLives = 3;
+    int _totalLives = 3;
+
+    int TotalLives
+    {
+        get => _totalLives;
+
+        set
+        {
+            _totalLives = value;
+            OnLivesChanged?.Invoke(_totalLives);
+        }
+    }
 
     private void OnEnable()
     {
         if (instance != null && instance != this)
         {
             Destroy(gameObject);
+            return;
         }
         else
         {
             instance = this;
             DontDestroyOnLoad(instance);
-        }
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-    {
-        switch(scene.buildIndex)
-        {
-            case 1:
-                player = GameObject.FindGameObjectWithTag("Player");
-                PlayerHealthSystem.OnPlayerDead += HandlePlayerDeath;
-                break;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
         }
     }
 
@@ -42,22 +48,54 @@ public class GameManager : MonoBehaviour
         GameStateManager.Instance.SetState(GameState.Gameplay);
     }
 
-    void HandlePlayerDeath()
+    private void OnApplicationQuit()
     {
-        totalLives--;
-        if (totalLives > 0)
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
+    private void OnSceneUnloaded(Scene scene)
+    {
+        if (scene.buildIndex == 1)
+        {
+            player = null;
+            PlayerHealthSystem.OnPlayerDead -= HandlePlayerDeath;
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    { 
+        switch(scene.buildIndex)
+        {
+            case 1:
+                TotalLives = 3;
+                player = GameObject.FindGameObjectWithTag("Player");
+                PlayerHealthSystem.OnPlayerDead += HandlePlayerDeath;
+                break;
+        }
+    }    
+
+    void HandlePlayerDeath(GameObject playerObject)
+    {
+        playerObject.SetActive(false);
+        TotalLives--;
+        
+        if (TotalLives > 0)
         {
             //Respawna
-            
+            RespawnManager.Instance.RespawnPlayer(playerObject);
         }
         else
         {
             //Gioco perso
+            GameStateManager.Instance.SetState(GameState.Lost);
+
+            OnGameLost?.Invoke();
         }
     }
 
-    private void OnDisable()
+    public void SetPlayer(bool canActivePlayer)
     {
-        PlayerHealthSystem.OnPlayerDead -= () => SceneManager.LoadScene(0);
+        player.GetComponent<PlayerHealthSystem>().enabled = canActivePlayer;
     }
 }
