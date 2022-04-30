94c800ed4afe69b5a3ff6ff7e479912f981c600a
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-04-30 12:16:15 +0200

    Adattato il metodo di gestione dell'evento in PlayerEquipment.cs quando si raccoglie il cappello

--- SheriffsInTown_Project/Assets/Scripts/PlayerEquipment.cs
+++ SheriffsInTown_Project/Assets/Scripts/PlayerEquipment.cs
@@ -35,7 +35,7 @@ public class PlayerEquipment : MonoBehaviour
         Pickup.OnHatPickupTaken -= ShowHat;
     }
 
-    private void ShowHat()
+    private void ShowHat(float newMovementSpeed, float newRunMovementSpeed)
     {
         hatRenderer.enabled = true;
     }
