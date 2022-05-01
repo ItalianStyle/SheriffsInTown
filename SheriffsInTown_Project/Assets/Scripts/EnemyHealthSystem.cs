483ed0b91f251c99478a19c0c3be9f28e4f53ad0
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-05-02 00:48:18 +0200

    Aggiunta la logica per gestire il boss nell'enemyHealthSystem.cs

--- SheriffsInTown_Project/Assets/Scripts/EnemyHealthSystem.cs
+++ SheriffsInTown_Project/Assets/Scripts/EnemyHealthSystem.cs
@@ -1,36 +1,46 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(EnemyAI))]
 public class EnemyHealthSystem : MonoBehaviour
 {
     public event Action OnEnemyDead = delegate { };
     public event Action<int, int> OnEnemyDamaged = delegate { };
+    public static event Action OnBossDead = delegate { };
+    public static event Action<int, int> OnBossDamaged = delegate { };
 
     [SerializeField] int maxHealth = 100;
     int _currentHealth = 100;
+    EnemyAI enemyAI;
 
     private void OnEnable()
     {
-        //TriggerTrap.OnPlayerTrap += (damage) => SetCurrentHealth(damage);
         _currentHealth = maxHealth;
     }
 
-    private void OnDisable()
+    private void Start()
     {
-        //TriggerTrap.OnPlayerTrap -= (damage) => SetCurrentHealth(damage);
+        enemyAI = GetComponent<EnemyAI>();   
     }
 
     public void TakeDamage(int amount)
     {
         _currentHealth -= amount;
-        OnEnemyDamaged?.Invoke(_currentHealth, maxHealth);        
+        if (enemyAI.enemyType is EnemyAI.EnemyType.Normal)
+            OnEnemyDamaged?.Invoke(_currentHealth, maxHealth);
+        
+        else
+            OnBossDamaged?.Invoke(_currentHealth, maxHealth);
 
         if (_currentHealth <= 0)
         {
-            //RespawnSystem.instance.ReloadScene();
-            OnEnemyDead?.Invoke();
-            Destroy(gameObject);
+            if (enemyAI.enemyType is EnemyAI.EnemyType.Normal)
+                OnEnemyDead?.Invoke();
+            
+            else
+                OnBossDead?.Invoke();
 
+            Destroy(gameObject);
             _currentHealth = maxHealth;
         }
     }
