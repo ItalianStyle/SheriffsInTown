eea3f52af59d719612c9d2289d1417f617b84211
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-05-02 00:49:47 +0200

    Aggiunta la logica per gestire il boss nell'EnemyAI

--- SheriffsInTown_Project/Assets/Scripts/EnemyAI.cs
+++ SheriffsInTown_Project/Assets/Scripts/EnemyAI.cs
@@ -1,33 +1,55 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAI : MonoBehaviour
 {
+    public static event Action OnPlayerEnteredBossArea = delegate { };
+
+    public enum EnemyType { Normal, Boss }
+    [Tooltip("In base a questo enum cambia il comportamento del nemico")]
+    public EnemyType enemyType;
+
     [Tooltip("A che distanza dal giocatore attacca")]
     [SerializeField] float attackDistance;
 
     [Tooltip("Tempo di ricarica prima di riattaccare")]
     [SerializeField] float attackReloadTime = 1f;
 
-    [Tooltip("Danno a colpo che infligge il nemico al giocatore")]
+    [Tooltip("Danno a colpo che infligge il nemico al giocatore.\nSi applica solo al nemico normale!\nOgni attacco speciale del boss ha il suo danno dedicato.")]
     [SerializeField] int damage = 20;
 
-    [Tooltip("Indica se questo nemico è il boss")]
-    [SerializeField] bool isBoss;
+    [Tooltip("Cosa può colpire il nemico con il raycast?")]
+    [SerializeField] LayerMask layerMask;
+
+    [SerializeField] Transform muzzleDX_Transform;
+    [SerializeField] Transform muzzleSX_Transform;
 
     bool playerDetected = false;
     PlayerHealthSystem playerHealth = null;
     NavMeshAgent agent = null;
     bool canShoot;
+
     Material enemyMaterial;
     Color initialColor;
 
+    BossAttackModule bossAttacks;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        enemyMaterial = transform.Find("Mesh").GetComponent<Renderer>().material;
+
+        if (enemyType is EnemyType.Boss)
+            bossAttacks = GetComponent<BossAttackModule>();
+    }
     private void Start()
     {
-        enemyMaterial = transform.Find("Mesh").GetComponent<Renderer>().material;
+
         initialColor = enemyMaterial.color;
-        agent = GetComponent<NavMeshAgent>();
+
         canShoot = true;
 
         PlayerHealthSystem.OnPlayerDead += HandlePlayerDeath;
@@ -50,27 +72,48 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
+        //Se il giocatore non è dentro l'area trigger non fare niente
         if (!(playerDetected && playerHealth))
             return;
-        
+
+        //Debug.Log(Vector3.Distance(playerHealth.transform.position, transform.position));
         if (Vector3.Distance(playerHealth.transform.position, transform.position) <= attackDistance)
         {
             agent.isStopped = true;
             if (canShoot)
             {
-                Ray ray = new Ray();
-                ray.origin = transform.position;
-                ray.direction = playerHealth.transform.position - transform.position;
+                if (enemyType is EnemyType.Normal)
+                {
+                    ShootFX_Manager.PlayMuzzleFlashFX(muzzleDX_Transform);
+                    Ray ray = new Ray
+                    {
+                        origin = transform.position,
+                        direction = playerHealth.transform.position - transform.position
+                    };
 
-                //if(Physics.Raycast(ray, attackDistance))//attack
-                playerHealth.CurrentHealth -= damage;
+                    if (Physics.Raycast(ray, out RaycastHit hitInfo, attackDistance, layerMask))//attack
+                    {
+                        ShootFX_Manager.PlayBulletHitFX(hitInfo.point);
+
+                        if (hitInfo.collider.CompareTag("Player"))
+                        {
+                            playerHealth.CurrentHealth -= damage;
+                        }
+                    }                
+                }
+                else
+                {
+                    //Se il nemico è il boss
+                    bossAttacks.Attack();
+                }
+                //Ricarica quando finisce l'attacco
                 StartCoroutine(Reload());
             }
-        }
-        else
-        {
-            agent.isStopped = false;
-            agent.SetDestination(playerHealth.transform.position);
+            else
+            {
+                agent.isStopped = false;
+                agent.SetDestination(playerHealth.transform.position);
+            }
         }
     }
 
@@ -86,6 +129,8 @@ public class EnemyAI : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             EngagePlayer(true, other);
+            if (enemyType is EnemyType.Boss)
+                OnPlayerEnteredBossArea?.Invoke();
         }
     }
 
