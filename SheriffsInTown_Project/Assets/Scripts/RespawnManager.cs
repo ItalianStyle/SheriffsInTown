6269b7edf7fe01a3e20609a1383b7e7795e18954
Italian_Style <lorenzo.petraroli@hotmail.it>
2022-05-06 12:20:57 +0200

    Aggiornato il RespawnManager.cs
    - Aggiunto al namespace SheriffsInTown
    - Adesso il player respawna all'ultimo checkpoint passato, non casuale

--- SheriffsInTown_Project/Assets/Scripts/RespawnManager.cs
+++ SheriffsInTown_Project/Assets/Scripts/RespawnManager.cs
@@ -4,36 +4,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class RespawnManager : MonoBehaviour
+namespace SheriffsInTown
 {
-    public static event Action OnPlayerRespawned = delegate { };
-
-    public static RespawnManager Instance;
-
-    GameObject[] spawnPoints;  //Lista di tutti gli spawn point della mappa
-    Transform spawnPoint = null;   //Il punto di spawn del player
-
-    void Start()
+    public class RespawnManager : MonoBehaviour
     {
-        Instance = this;
+        public static event Action OnPlayerRespawned = delegate { };
 
-        //Inizializzo la lista dei punti di spawn della mappa
-        spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
-    }
+        public static RespawnManager Instance;
 
-    void Update()
-    {
-        
-    }
+        GameObject[] spawnPoints;  //Lista di tutti gli spawn point della mappa
+        GameObject lastCheckpointSaved;     //L'ultimo checkpoint salvato dal player
 
-    public void RespawnPlayer(GameObject player)
-    {
-        //Seleziona un punto di spawn casuale
-        spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length - 1)].transform;
-        //Posiziona il player
-        player.transform.position = spawnPoint.position;
+        private void Awake()
+        {
+            Instance = this;
+            //Inizializzo la lista dei punti di spawn della mappa
+            spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        }
+        void Start()
+        {
+            lastCheckpointSaved = spawnPoints[0];
+            CheckpointTrigger.OnPlayerEnteredCheckpointTrigger += SaveCheckpoint;
+        }
 
-        player.SetActive(true);
-        OnPlayerRespawned?.Invoke();
+        private void OnDestroy()
+        {
+            CheckpointTrigger.OnPlayerEnteredCheckpointTrigger -= SaveCheckpoint;
+        }
+
+        private void SaveCheckpoint(GameObject checkpoint)
+        {
+            //Trova il checkpoint passato nella lista
+            foreach(GameObject spawnPoint in spawnPoints)
+            {
+                if(spawnPoint.name == checkpoint.name)
+                {
+                    lastCheckpointSaved = checkpoint;
+                    Debug.Log("Checkpoint salvato con successo");
+                    break;
+                }
+            }
+        }
+
+        public void RespawnPlayer(GameObject player)
+        {
+            //Posiziona il player
+            player.transform.position = lastCheckpointSaved.transform.position;
+
+            player.SetActive(true);
+            OnPlayerRespawned?.Invoke();
+        }
     }
 }
